adcc19e3bde236550b0be850b9a27606c306a7d5
Anant Choudhari <ac6413@nyu.edu>
2018-03-09 22:23:16 -0500

    Create RemoveVowels.py

---
+++ CoderWars/RemoveVowels.py
@@ -0,0 +1,16 @@
+"""
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+"""
+
+def disemvowel(string):
+    vows=['a','e','i','o','u']
+    chars=[]
+    for i in string:
+        if i.lower() not in vows:
+            chars.append(i)
+    return "".join(chars)
