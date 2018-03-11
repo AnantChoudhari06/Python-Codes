659b6b52f6f7bc9d3c5f038f8ddf3e8adaec5f06
Anant Choudhari <ac6413@nyu.edu>
2018-03-10 19:17:03 -0500

    Create BinaryAddition.py

---
+++ CoderWars/BinaryAddition.py
@@ -0,0 +1,9 @@
+"""
+Implement a function that adds two numbers together and returns their sum in binary. 
+The conversion can be done before, or after the addition.
+
+"""
+
+def add_binary(a,b):
+    #your code here
+    return (bin(a+b)[2:])
